853d313748766502d96f592e688e674cca45a1fd
Video <videogamesm12@gmail.com>
2019-05-10 11:50:35 -0600

    enhanced /dialup

--- src/com/phantomoediv215/phantomtools/commands/Command_dialup.java
+++ src/com/phantomoediv215/phantomtools/commands/Command_dialup.java
@@ -5,14 +5,12 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
-
 public class Command_dialup extends BukkitCommand {   
 
     @Override
     public boolean run(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-        sender.sendMessage(ChatColor.BLUE + "You've got mail!");
-        sender.sendMessage(ChatColor.DARK_BLUE + "You need to know what I mean on that to get the joke.");
+        sender.sendMessage(ChatColor.BLUE + "Welcome!");
+        sender.sendMessage(ChatColor.DARK_BLUE + "You've got mail!");
         return true;
-    
     }
 }
